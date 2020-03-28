deee04f3213731a5791bf1e0d5855ed892a93559
Kopano Monyobo <59963220+kopano-m@users.noreply.github.com>
2020-03-28 16:21:44 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Package to extract data from EDSA created database',
+    long_description=open('README.md').read(),
+    install_requires=['numpy','sqlalchemy','botocore','json'],
+    url='https://github.com/kopano-m/database.git',
+    author='Kopano Monyobo via EDSA',
+    author_email='kopanomonyobo@gmail.com'
+)
